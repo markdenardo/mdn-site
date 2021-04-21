ba25d056126daf5a941cd6d71ad00effadc03213
markdenardo <mark.denardo@gmail.com>
2021-04-21 17:29:27 -0400

    body

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Vimeo from "@u-wave/react-vimeo";
 import Header from "./components/Header";
 import P5Wrapper from "react-p5-wrapper";
 import sketch from "./components/Sketch";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
-const videos = ["https://vimeo.com/532356163"]
+const videos = ["https://vimeo.com/532356163"];
 
 function App() {
   return (
@@ -14,7 +14,9 @@ function App() {
         <Header />
         <Vimeo className="vimeo" video={videos[0]} />
       </header>
-      <footer><Footer/></footer>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,15 @@ const Header = () => {
       <h3>composer, designer, educator</h3>
       <p>
         I’m a Software Engineer with experience in Sound Design, Game
-        Development, and Education. Currently working in React Development,
-        Design / Development of Music Software, and Composition / Sound Design
-        in Game Design Frameworks. Interested in forging new partnerships with
-        creative companies who build creative tools, equitable communities, and
-        sustainable services. 15+ Experience in Game Design; 5+ years Experience
-        in STEM Education; 3+ years Experience in Full Stack Development{" "}
+        Development, and Education.
+        <br></br>Currently working in React Development, Design / Development of
+        Music Software, and Composition / Sound Design in Game Design
+        Frameworks.
+        <br></br>Interested in forging new partnerships with creative companies
+        who build creative tools, equitable communities, and sustainable
+        services. <br></br>15+ Experience in Game Design; <br></br>5+ years
+        Experience in STEM Education; <br></br>3+ years Experience in Full Stack
+        Development{" "}
       </p>
     </div>
   );
